9c1486b5aa37d2eec12168e009e6fb18d5031e73
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-19 18:20:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  end
+  for x in (2..Math.sqrt(number))
+    if number % x == 0
+      return false
+    end
+  end
+  return true
+end
