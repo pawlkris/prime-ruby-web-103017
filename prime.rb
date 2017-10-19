1bc957c6fcef402b2d7b189c3c7ff790c48fb3a2
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-19 18:04:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # Add  code here!
 def prime?(number)
-  if number % 2 == 0
+  if number <= 3
+    return true
+  elsif number % 2 == 0
     return false
   elsif number % 3 == 0
     return false
-  else 
+  else
     return true
   end
 end
